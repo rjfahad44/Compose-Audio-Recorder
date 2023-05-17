ab122f2008619a2e5b8687f1f3aa64993a6da7e3
Fahad_2022 <rjfahad44@gmail.com>
2023-05-17 21:51:14 +0600

    update

---
+++ app/src/main/java/com/ft/audiorecorder/MainActivity.kt
@@ -0,0 +1,73 @@
+package com.ft.audiorecorder
+
+import android.Manifest
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.core.app.ActivityCompat
+import com.ft.audiorecorder.playback.AndroidAudioPlayer
+import com.ft.audiorecorder.record.AndroidAudioRecorder
+import com.ft.audiorecorder.ui.theme.AudioRecorderTheme
+import java.io.File
+
+class MainActivity : ComponentActivity() {
+
+    private val recorder by lazy {
+        AndroidAudioRecorder(applicationContext)
+    }
+
+    private val player by lazy {
+        AndroidAudioPlayer(applicationContext)
+    }
+
+    private var audioFile: File? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.RECORD_AUDIO),
+            0
+        )
+        setContent {
+            AudioRecorderTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        File(cacheDir, "audio.mp3").also {
+                            recorder.start(it)
+                            audioFile = it
+                        }
+                    }) {
+                        Text(text = "Start recording")
+                    }
+                    Button(onClick = {
+                        recorder.stop()
+                    }) {
+                        Text(text = "Stop recording")
+                    }
+                    Button(onClick = {
+                        player.playFile(audioFile ?: return@Button)
+                    }) {
+                        Text(text = "Play")
+                    }
+                    Button(onClick = {
+                        player.stop()
+                    }) {
+                        Text(text = "Stop playing")
+                    }
+                }
+            }
+        }
+    }
+}
